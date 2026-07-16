2029b3aceb8506fd224b75f0f23368ff95d6a961
RiteshVerma1102 <vermaritesh24106867@gmail.com>
2026-07-16 11:16:10 +0530

    [2938] [Time Beats: 12.87%] [Memory Beats: 85.93%] - LeetPush

--- 2938-separate-black-and-white-balls/2938-Separate-Black-and-White-Balls.java
+++ 2938-separate-black-and-white-balls/2938-Separate-Black-and-White-Balls.java
@@ -1,12 +1,30 @@
 class Solution {
     public long minimumSteps(String s) {
         long ans = 0;
-        int zeroPos = 0;
+        // int zeroPos = 0;
+        int n = s.length();
+        int i =0; 
+        int j = n - 1;
 
-        for(int i =0; i < s.length();i++){
-            if(s.charAt(i) == '0'){
-                ans += (i - zeroPos);
-                zeroPos++;
+        // for(int i =0; i < s.length();i++){
+        //     if(s.charAt(i) == '0'){
+        //         ans += (i - zeroPos);
+        //         zeroPos++;
+        //     }
+        // }
+        // return ans;
+        while(i < j){
+            if(i  < j && s.charAt(i) == '0'){
+                i++;
+            }
+            else if(i < j && s.charAt(j) == '1'){
+                j--;
+            
+            }
+            else{
+                ans +=(j - i);
+                i++;
+                j--;
             }
         }
         return ans;
