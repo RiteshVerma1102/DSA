cda299a844c399a7b6d9651c838876d5fa86e293
RiteshVerma1102 <vermaritesh24106867@gmail.com>
2026-07-16 11:52:15 +0530

    [3223] [Time Beats: 96.33%] [Memory Beats: 82.11%] - LeetPush

--- 3223-minimum-length-of-string-after-operations/3223-Minimum-Length-of-String-After-Operations.java
+++ 3223-minimum-length-of-string-after-operations/3223-Minimum-Length-of-String-After-Operations.java
@@ -1,25 +1,27 @@
 class Solution {
     public int minimumLength(String s) {
-        int[]  freq= new int[26];
-        int i = 0;
 
-        while(i < s.length()){  
+        int[] freq = new int[26];
+
+        // Count frequency
+        for (int i = 0; i < s.length(); i++) {
             freq[s.charAt(i) - 'a']++;
-            i++;
         }
-        int ans =0;
-        i = 0;
-        while(i < 26){
-            if(freq[i] != 0){
-                if(freq[i] % 2 == 0)
-                    ans += 2;
-                else
-                    ans += 1;
 
-                }
-                i++;
+        int ans = 0;
 
-            }
-            return ans;
+        // Calculate remaining characters
+        for (int i = 0; i < 26; i++) {
+
+            if (freq[i] == 0)
+                continue;
+
+            if (freq[i] % 2 == 0)
+                ans += 2;
+            else
+                ans += 1;
+        }
+
+        return ans;
     }
 }
